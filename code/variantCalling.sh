fe07297c8c98e870334dddacd03c0b8f318b90b9
guerreiroduarte <guerrreiroduarteufpa@gmail.com>
2026-07-22 18:17:23 -0300

    Add phasing

--- code/variantCalling.sh
+++ code/variantCalling.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
-MODEL_LINK="https://www.bio8.cs.hku.hk/clair3/clair3_models_pytorch/r1041_e82_400bps_sup_v430_bacteria_finetuned/"
-MODEL_NAME="r1041_e82_400bps_sup_v430_bacteria_finetuned"
 SEED="20030407"
-MAX_DEPTH="5000"
+MAX_DEPTH=5000
 
-while getopts "i:o:r:t:d:s:" opt; do
+while getopts "i:o:r:t:" opt; do
     case "$opt" in
         i) ALIGNED_DIR="$OPTARG" ;; # Directory with the input zipped reads in fastq format
         o) OUTPUT_DIR="$OPTARG"  ;; # Directory in which the processed reads will be saved
         r) REF="$OPTARG"         ;; # Reference targets in fasta format
         t) TARGETS="$OPTARG"     ;; # Bed file of targets to call variants on
-        d) MAX_DEPTH="$OPTARG"   ;;
         s) SEED="$OPTARG"        ;;
+        d) MAX_DEPTH="$OPTARG"   ;;
         *) echo "Invalid option"; exit 1 ;;
     esac
 done
@@ -28,11 +26,19 @@ echo -e "SAMPLE\tAMPLICON\tPOS\tREF\tALT\tQUAL\tDP\tDP4\tIs_Ti\tIs_Ts" > "$VAR_S
 for read in "$ALIGNED_DIR"/*.bam; do
     sample_name=$(basename "$read" .bam)
     var_file="$OUTPUT_DIR/${sample_name}_vars.vcf"
+
+    phased_var="$OUTPUT_DIR/${sample_name}_phased"
+    tagged_bam="$OUTPUT_DIR/${sample_name}_tagged"
     
     if [ ! -f "$var_file" ]; then
         echo "Calling variants for $sample_name..."
+
         bcftools mpileup -Ov -B -q 15 -Q 10 -R "$TARGETS" -f "$REF" -d "$MAX_DEPTH" --seed "$SEED" "$read" | \
-            bcftools call --ploidy 2 -V indels -mv -P 0.1 -o "$var_file"
+            bcftools call -V indels -mv -P 0.1 -o "$var_file"
+
+        longphase phase -s "$var_file" -b "$read" -r "$REF" -o "$phased_var" --ont
+
+        longphase haplotag -r "$REF" -s "$phased_var".vcf -b "$read" -o "$tagged_bam"
     else
         echo "Variants for $sample_name were already called."
     fi
